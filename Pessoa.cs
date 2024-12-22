67f004883cbdfbda2692b122778900f7f4258fa2
gothwavy <ryanalmeidadev@gmail.com>
2024-12-22 18:02:53 -0300

    Finalizado✅

---
+++ Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_GetSet
+{
+    internal class Pessoa
+    {
+        // Atributo nome
+        private string nome;
+
+        // Get e Set
+        public string Nome
+        {
+            //Get -> Usado para pegar uma informacao
+            get
+            {
+                return nome;
+            }
+
+            //Set -> Usado para enviar uma informacao
+            set
+            {
+                nome = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _08_GetSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Ryan";
+            Console.WriteLine(p.Nome);
+            // Os metodos get e set servem para que o objeto tenha acesso a um atributo privado de forma com que o atributo nao deixe de ser privado. ele pode alterar o valor do atributo com set, pois, estara enviando uma informacao para o atributo, ja com o get, ele apenas consulta o atributo e pega esse valor.
+        }
+    }
+}
